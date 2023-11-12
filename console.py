446b51f9d359961b90caefb7cd57c3e4083af926
Leul <leulassefa255@gmail.com>
2023-11-12 07:31:25 -0800

    AirBnB clone  The console

--- console.py
+++ console.py
@@ -30,9 +30,5 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
-    def emptyline(self):
-        """Do nothing upon receiving an empty line."""
-        pass
-
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
